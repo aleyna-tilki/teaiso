607bb8e4c059fd878351b6590cbaa83bef3f7203
sulincix <parduscix@yandex.ru>
2022-12-07 07:50:13 +0000

    Update customize-airootfs.sh

--- aleyna/customize-airootfs.sh
+++ aleyna/customize-airootfs.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+cd /tmp
 
-echo "Nothing to do!"
+### Instally 17g
+apt install wget
+wget https://github.com/The-Aleyna-Tilki-Linux/17g-installer/releases/download/current/17g-installer_1.0_all.deb
+apt install ./*.deb -yq --allow-downgrades
+
+#### Disable recommends by default
+cat > /etc/apt/apt.conf.d/01norecommend << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
