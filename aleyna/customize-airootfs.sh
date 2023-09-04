3b361fed480c3d25f0c7098f33476d512b7ce7cf
sulincix <parduscix@yandex.ru>
2023-09-04 07:57:02 +0000

    Update customize-airootfs.sh

--- aleyna/customize-airootfs.sh
+++ aleyna/customize-airootfs.sh
@@ -4,7 +4,7 @@ cd /tmp
 ### Instally 17g and other stuff
 apt install wget
 wget https://github.com/aleyna-tilki/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
-wget https://github.com/The-Aleyna-Tilki-Linux/17g-installer/releases/download/current/17g-installer_1.0_all.deb
+https://github.com/aleyna-tilki/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 wget https://github.com/pardus-nosystemd/desktop-base/releases/download/current/desktop-base_9999-noupdate_all.deb
 wget https://github.com/aleyna-tilki/aleyna-theme/releases/download/current/aleyna-theme.deb
 apt install ./*.deb -yq --allow-downgrades
