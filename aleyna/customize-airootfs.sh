243ac7d7be4daf4971174b19cc490e662fc5f7b8
sulincix <parduscix@yandex.ru>
2024-07-27 21:52:33 +0000

    Update customize-airootfs.sh

--- aleyna/customize-airootfs.sh
+++ aleyna/customize-airootfs.sh
@@ -9,7 +9,7 @@ yes | apt install wget -yq
 wget https://github.com/aleyna-tilki/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
 wget https://github.com/aleyna-tilki/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 wget https://github.com/pardus-nosystemd/desktop-base/releases/download/current/desktop-base_9999-noupdate_all.deb
-wget https://github.com/aleyna-tilki/aleyna-theme/releases/download/current/aleyna-theme.deb
+wget https://github.com/aleyna-tilki/aleyna-theme/releases/download/current/aleyna-theme_1.0_amd64.deb
 wget https://github.com/aleyna-tilki/pipewire-launcher/releases/download/current/pipewire-launcher_1.0.0_all.deb
 yes | apt install ./*.deb -yq --allow-downgrades
 
